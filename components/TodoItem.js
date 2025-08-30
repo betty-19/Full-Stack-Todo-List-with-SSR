9808f65ef372b0cfe18b8007bd4f8e6d36b317b2
Betelhem <betelhemsintayehu10@gmail.com>
2025-08-30 09:30:00 +0300

    update TodoItem page

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function TodoItem({ todo }) {
   return (
-    <li className={`border p-4 rounded shadow flex justify-between items-center bg-white ${todo.completed ? 'line-through text-gray-500' : 'text-black-500'}`}>
-      <span>{todo.title}</span>
+    <li className={`border p-4 rounded shadow flex justify-between items-center bg-white ${todo.completed ? 'line-through text-gray-500' : ''}`}>
+      <span className={` ${todo.completed ? 'text-gray-500' : 'text-green-500 font-bold'}`}>{todo.title}</span>
       <div>
         <Link href={`/${todo.id}`} className="text-blue-600 hover:underline ml-4">Edit</Link>
       </div>
